b8cc0b7684660757ed1afff8be7d9132138fe64b
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-07-26 23:14:52 -0700

    begin loop component prototype

--- src/components/TimelineView.js
+++ src/components/TimelineView.js
@@ -2,12 +2,61 @@ import React from "react";
 
 const TimelineView = ({elements}) => {
 
-    console.log(elements)
+      const entrance = [0, 400];
+      const controlPointEntranceToRight = [275, 400];
+
+      const right = [300, 200];
+      const controlPointRightToTop = [290, 100];
+      
+      const top = [200, 100];
+      const controlPointTopToLeft = [110, 100];
+      
+      const left = [100, 200];
+      const controlPointLeftToExit = [125, 400];
+      
+      const exit = [400, 400];
+      
+      const Point = ({coordinates, color}) => {
+
+        let [cx, cy] = coordinates;
+        let radius = 10;
+
+        return <ellipse cx={cx} cy={cy} rx={radius} ry={radius} fill={color} />;
+
+      }
+
+      const loop = (
+        <>
+          <path
+            d={`
+              M ${entrance}
+              Q ${controlPointEntranceToRight} ${right}
+              Q ${controlPointRightToTop} ${top}
+              Q ${controlPointTopToLeft} ${left}
+              Q ${controlPointLeftToExit} ${exit}
+              
+            `}
+            fill="none"
+            stroke="red"
+            strokeWidth={5} />
+          
+            <Point coordinates={entrance} color="#666666" />
+            <Point coordinates={right} color="#666666" />
+            <Point coordinates={top} color="#666666" />
+            <Point coordinates={left} color="#666666" />
+            <Point coordinates={exit} color="#666666" />
+
+            <Point coordinates={controlPointEntranceToRight} color="#FFCCCC" />
+            <Point coordinates={controlPointRightToTop} color="#FFCCCC" />
+            <Point coordinates={controlPointTopToLeft} color="#FFCCCC" />
+            <Point coordinates={controlPointLeftToExit} color="#FFCCCC" />
+          </>
+      );
 
     return (
-        <>
-            <p>test</p>
-        </>
+        <svg viewBox="0 0 800 800">
+            {loop}
+        </svg>
     );
 
 }
