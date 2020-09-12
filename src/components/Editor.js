19287a1814aa379ffaccdfd6b45a2de7e75becfd
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-09-11 23:47:44 -0700

    add divs for editor layout

---
+++ src/components/Editor.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const Editor = () => {
+
+  return (
+    <>
+      <div className="editorContainer">
+
+          <div className="editorSidebar">
+
+            <div className="partsPicker">
+
+            </div>
+
+            <div className="trackTypePicker">
+                
+            </div>
+
+          </div>
+
+          <div className="editorMain">
+
+            <div className="timelineView">
+
+            </div>
+
+            <div className="currentPartContainer">
+
+                <div className="currentPartPreview">
+                
+                </div>
+
+                <div className="currentPartSettings">
+
+                </div>
+                
+            </div>
+
+          </div>
+
+      </div>
+
+    </>
+  );
+  }
+
+export default Editor;
